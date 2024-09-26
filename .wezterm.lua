5eb23e9452b9bd9c7ee6010993a439788651ddd5
Baichuan Huang <baichuan55555@gmail.com>
2024-09-26 19:13:20 -0400

    Update .wezterm.lua

--- .wezterm.lua
+++ .wezterm.lua
@@ -6,6 +6,16 @@ local config = wezterm.config_builder()
 
 -- This is where you actually apply your config choices
 
+config.enable_scroll_bar = true
+
+config.keys = {
+  {
+    key = 'w',
+    mods = 'CTRL|SHIFT',
+    action = wezterm.action.CloseCurrentPane { confirm = true },
+  },
+}
+
 -- For example, changing the color scheme:
 config.color_scheme = 'Snazzy (Gogh)'
 
